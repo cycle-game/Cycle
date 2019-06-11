c22fd854c68a81f563d7f8dc3f50c11772c00f91
Gaëtan Maisse <gmaisse@solocal.com>
2019-06-11 22:24:16 +0200

    chore: add `.editorconfig` file and format code

--- src/Etats/scores.js
+++ src/Etats/scores.js
@@ -1,83 +1,84 @@
-GameState.Scores = function (game) { };
+GameState.Scores = function (game) {
+};
 
 GameState.Scores.prototype = {
-    create: function() {
-        
-        var highscores = scores().split('|||');
-        
-        var label = cliquable(c/2, plataille, lang.UnSeul, 22, 0.5, 0.5, 0, 700, null, this);
-        
-        // Retour au menu
-        this.esc_key = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
-        
-        // Difficulté
-        this.choix_diff = [];
-        difficultes(this);
-        
-        if (isNaN(Stats.diff))
-            Stats.diff = 1;
-        
-        this.diff = Stats.diff;
-        
-        // Affichage des scores
-        this.scores = [];
-        var margey = 70;
-        var margex = 150;
-        
-        for (var val in this.choix_diff) {
-            
-            this.scores[val] = this.game.add.group();
-            
-            var specific_highscores = highscores[val].split('><');
-            var espace_y = ecart(specific_highscores.length, margey);
-            
-            // Parcourt en y
-            for (var i = 0; i < specific_highscores.length; i++) {
-                
-                var specific_player = specific_highscores[i].split('::');
-                var espace_x = ecart(specific_player.length, margex);
-                
-                // Parcourt en x
-                for (var j = 0; j < specific_highscores.length; j++) {
-                    var tmp = cliquable(margex + j * espace_x,
-                                        margey + i * espace_y,
-                                        specific_player[j],
-                                        25, 0.5, 0.5, 0, null, null, this);
-                    tmp.alpha = 1;
-                    this.scores[val].add(tmp);
-                }
-            }
-            
-            //this.scores[val] = cliquable(c/2, c/2, highscores[val], 25, 0.5, 0.5, 0, null, null, this);
-            
-            if (val == this.diff)
-                this.scores[val].alpha = 1;
-            else
-                this.scores[val].alpha = 0;
-            
+  create: function () {
+
+    var highscores = scores().split('|||');
+
+    var label = cliquable(c / 2, plataille, lang.UnSeul, 22, 0.5, 0.5, 0, 700, null, this);
+
+    // Retour au menu
+    this.esc_key = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+
+    // Difficulté
+    this.choix_diff = [];
+    difficultes(this);
+
+    if (isNaN(Stats.diff))
+      Stats.diff = 1;
+
+    this.diff = Stats.diff;
+
+    // Affichage des scores
+    this.scores = [];
+    var margey = 70;
+    var margex = 150;
+
+    for (var val in this.choix_diff) {
+
+      this.scores[val] = this.game.add.group();
+
+      var specific_highscores = highscores[val].split('><');
+      var espace_y = ecart(specific_highscores.length, margey);
+
+      // Parcourt en y
+      for (var i = 0; i < specific_highscores.length; i++) {
+
+        var specific_player = specific_highscores[i].split('::');
+        var espace_x = ecart(specific_player.length, margex);
+
+        // Parcourt en x
+        for (var j = 0; j < specific_highscores.length; j++) {
+          var tmp = cliquable(margex + j * espace_x,
+            margey + i * espace_y,
+            specific_player[j],
+            25, 0.5, 0.5, 0, null, null, this);
+          tmp.alpha = 1;
+          this.scores[val].add(tmp);
         }
-        
-        this.choix_diff[Stats.diff].setShadow(0, 0, 'rgba(0, 0, 0, 1)', 5);
-        
-    },
-    souligne: function(sprite) {
-        
-        // Selection
-        for (var val in this.choix_diff) {
-            this.choix_diff[val].setShadow(0, 0, 'rgba(0, 0, 0, 0)', 5);
-            this.scores[val].alpha = 0;
-        }
-        
-        sprite.setShadow(0, 0, 'rgba(0, 0, 0, 1)', 5);
-        this.diff = sprite.diff;
-        this.scores[this.diff].alpha = 1;
-        
-    },
-    update: function() {
-        
-        // Retour au menu
-        if (this.esc_key.isDown)
-            this.game.state.start('Menu');
-        
+      }
+
+      //this.scores[val] = cliquable(c/2, c/2, highscores[val], 25, 0.5, 0.5, 0, null, null, this);
+
+      if (val == this.diff)
+        this.scores[val].alpha = 1;
+      else
+        this.scores[val].alpha = 0;
+
     }
+
+    this.choix_diff[Stats.diff].setShadow(0, 0, 'rgba(0, 0, 0, 1)', 5);
+
+  },
+  souligne: function (sprite) {
+
+    // Selection
+    for (var val in this.choix_diff) {
+      this.choix_diff[val].setShadow(0, 0, 'rgba(0, 0, 0, 0)', 5);
+      this.scores[val].alpha = 0;
+    }
+
+    sprite.setShadow(0, 0, 'rgba(0, 0, 0, 1)', 5);
+    this.diff = sprite.diff;
+    this.scores[this.diff].alpha = 1;
+
+  },
+  update: function () {
+
+    // Retour au menu
+    if (this.esc_key.isDown)
+      this.game.state.start('Menu');
+
+  }
 };
--- src/Etats/victoire.js
+++ src/Etats/victoire.js
@@ -1,86 +1,85 @@
-
 // On crée l'état du menu
-GameState.Victoire = function (game) { };
+GameState.Victoire = function (game) {
+};
 
 GameState.Victoire.prototype = {
-    create: function() {
-        
-        var label = cliquable(c/2, c/4, lang.Victoire+"\n\n"+Math.round(Stats.score), 25, 0.5, 0.5, 0, 500, null, this);
-        
-        var enter_pseudo = cliquable(c/2, c/2, lang.Pseudo, 25, 0.5, 0.5, 300, 500, null, this);
-        
-        this.pseudo = cliquable(c/2, (c/4)*3, pseudo+'_', 25, 0.5, 0.5, 0, 500, null, this);
-        
-        // Retour au menu
-        this.esc_key = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
-        
-        // Clavier
-        this.game.input.keyboard.addCallbacks( this, this.tape );
-        
-        // Retour
-        var back = this.game.input.keyboard.addKey(Phaser.Keyboard.BACKSPACE);
+  create: function () {
 
-        save({level: 0, score: 0, diff: Stats.diff}); 
-        
-    },
-    addToPseudo: function(letter) {
-        
-        // Écriture du pseudo
-        
-        if (pseudo.length <= 16)
-            pseudo = pseudo+letter;
-        
-        if (pseudo.length < 16)
-            this.pseudo.text = pseudo+"_";
-        else
-            this.pseudo.text = pseudo;
-        
-    },
-    tape: function(evt) {
-        
-        if( evt.which == Phaser.Keyboard.BACKSPACE ) {
-            
-            // deleteToPseudo
-            if (pseudo.length > 0) {
-                pseudo = pseudo.slice(0, -1);
-                this.pseudo.text = pseudo+"_";
-            }
-            
-            return;
-        }
-        
-        if( evt.which == Phaser.Keyboard.ENTER ) {
-            
-            if (pseudo.length > 0) {
-                toHigh(Stats, pseudo);
-                sociaux(Stats, pseudo, lang);
-                reset(Stats);
-                this.game.state.start('Scores');
-            }
-            
-            return;
-            
-        }
-        
-        // http://www.html5gamedevs.com/topic/4068-text-input-from-users-in-phaser/
-        if( evt.which < "A".charCodeAt(0) || evt.which > "Z".charCodeAt(0) )
-        {
-            //console.log( "Not a letter: ", evt.which );
-            return;
-        }
-        
-        var letter = String.fromCharCode( evt.which );
-        if( !evt.shiftKey ) letter = letter.toLowerCase();
-        
-        this.addToPseudo(letter);
-    },
-    update: function() {
-        
-        // Retour au menu
-        if (this.esc_key.isDown) {
-            reset(Stats);
-            this.game.state.start('Menu');
-        }
-        
+    var label = cliquable(c / 2, c / 4, lang.Victoire + "\n\n" + Math.round(Stats.score), 25, 0.5, 0.5, 0, 500, null, this);
+
+    var enter_pseudo = cliquable(c / 2, c / 2, lang.Pseudo, 25, 0.5, 0.5, 300, 500, null, this);
+
+    this.pseudo = cliquable(c / 2, (c / 4) * 3, pseudo + '_', 25, 0.5, 0.5, 0, 500, null, this);
+
+    // Retour au menu
+    this.esc_key = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+
+    // Clavier
+    this.game.input.keyboard.addCallbacks(this, this.tape);
+
+    // Retour
+    var back = this.game.input.keyboard.addKey(Phaser.Keyboard.BACKSPACE);
+
+    save({level: 0, score: 0, diff: Stats.diff});
+
+  },
+  addToPseudo: function (letter) {
+
+    // Écriture du pseudo
+
+    if (pseudo.length <= 16)
+      pseudo = pseudo + letter;
+
+    if (pseudo.length < 16)
+      this.pseudo.text = pseudo + "_";
+    else
+      this.pseudo.text = pseudo;
+
+  },
+  tape: function (evt) {
+
+    if (evt.which == Phaser.Keyboard.BACKSPACE) {
+
+      // deleteToPseudo
+      if (pseudo.length > 0) {
+        pseudo = pseudo.slice(0, -1);
+        this.pseudo.text = pseudo + "_";
+      }
+
+      return;
     }
+
+    if (evt.which == Phaser.Keyboard.ENTER) {
+
+      if (pseudo.length > 0) {
+        toHigh(Stats, pseudo);
+        sociaux(Stats, pseudo, lang);
+        reset(Stats);
+        this.game.state.start('Scores');
+      }
+
+      return;
+
+    }
+
+    // http://www.html5gamedevs.com/topic/4068-text-input-from-users-in-phaser/
+    if (evt.which < "A".charCodeAt(0) || evt.which > "Z".charCodeAt(0)) {
+      //console.log( "Not a letter: ", evt.which );
+      return;
+    }
+
+    var letter = String.fromCharCode(evt.which);
+    if (!evt.shiftKey) letter = letter.toLowerCase();
+
+    this.addToPseudo(letter);
+  },
+  update: function () {
+
+    // Retour au menu
+    if (this.esc_key.isDown) {
+      reset(Stats);
+      this.game.state.start('Menu');
+    }
+
+  }
 };
