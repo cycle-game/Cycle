1705c389e9e97cb09bb11bb66d9c1904b885ef76
Gaëtan Maisse <gmaisse@solocal.com>
2019-06-25 17:30:47 +0200

    chore: add babel for ES "transpilation"

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,17 @@ module.exports = {
     rules: [
       { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
       { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-      { test: /p2\.js/, use: ['expose-loader?p2'] }
+      { test: /p2\.js/, use: ['expose-loader?p2'] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ]
   },
   resolve: {
