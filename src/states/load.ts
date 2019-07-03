8d471605e869979f0590b944e162d85aa4fd0e08
Rémi PERROT <rperrot@leadformance.com>
2019-07-03 13:31:26 +0200

    refactor: split Phaser folder to tiles & sprites
    See PR #13

--- src/states/load.ts
+++ src/states/load.ts
@@ -30,25 +30,23 @@ export const Load = {
         // Celui du dessus sera révélé petit à petit
         this.game.load.setPreloadSprite(preloading1);
 
-        // Et pendant se temps on charge toutes les ressources !
+        // Et pendant ce temps on charge toutes les ressources !
 
-        this.game.load.image(PLAYER, 'resources/Phaser/player.png');
-        this.game.load.image(SELECTOR, 'resources/Phaser/selector.png');
-        this.game.load.image(NIGHT_MASK, 'resources/Phaser/night-mask.png');
-        this.game.load.image(PLANET, 'resources/Phaser/planet.svg');
+        this.game.load.image(LANG, 'resources/tiles/lang.png');
+        this.game.load.image(LOGO_C_MASK, 'resources/tiles/logo-c-mask.png');
+        this.game.load.image(LOGO_WITHOUT_C, 'resources/tiles/logo-without-c.png');
+        this.game.load.image(PLANET, 'resources/tiles/planet.svg');
+        this.game.load.image(PLATFORM, 'resources/tiles/platform.png');
+        this.game.load.image(TRAP, 'resources/tiles/trap.png');
 
-        this.game.load.image(PLATFORM, 'resources/Phaser/platform.png');
-        this.game.load.image(STAR, 'resources/Phaser/star.png');
-        this.game.load.image(TRAP, 'resources/Phaser/trap.png');
+        this.game.load.image(LANG_SELECTOR, 'resources/sprites/lang-selector.png');
+        this.game.load.image(LOGO_C, 'resources/sprites/logo-c.png');
+        this.game.load.image(NIGHT_MASK, 'resources/sprites/night-mask.png');
+        this.game.load.image(PLAYER, 'resources/sprites/player.png');
+        this.game.load.image(SELECTOR, 'resources/sprites/selector.png');
+        this.game.load.image(STAR, 'resources/sprites/star.png');
 
-        this.game.load.image(LANG, 'resources/Phaser/lang.png');
-        this.game.load.image(LANG_SELECTOR, 'resources/Phaser/lang-selector.png');
-
-        this.game.load.image(LOGO_WITHOUT_C, 'resources/Phaser/logo-without-c.png');
-        this.game.load.image(LOGO_C, 'resources/Phaser/logo-c.png');
-        this.game.load.image(LOGO_C_MASK, 'resources/Phaser/logo-c-mask.png');
-
-        this.game.load.image(DEBUG_POINT, 'resources/Phaser/debug-point.png');
+        this.game.load.image(DEBUG_POINT, 'resources/sprites/debug-point.png');
     },
     create: function() {
         // Et on passe à la sélection de langue
