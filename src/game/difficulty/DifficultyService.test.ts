ef09b564a2c060fef147fa16d5000db76ffbcf38
Gaëtan Maisse <gmaisse@solocal.com>
2019-07-22 13:15:19 +0200

    refactor: introduce a service to manage difficulty

---
+++ src/game/difficulty/DifficultyService.test.ts
@@ -0,0 +1,14 @@
+import { difficultyService, DifficultyService } from './DifficultyService';
+import { easy, hard, normal } from './Difficulty';
+
+describe(DifficultyService.prototype.constructor.name, () => {
+    it('has default value set to easy', () => {
+        expect(difficultyService.selectedDifficulty()).toEqual(easy);
+    });
+
+    it('store current difficulty', () => {
+        difficultyService.selectDifficulty(normal);
+        difficultyService.selectDifficulty(hard);
+        expect(difficultyService.selectedDifficulty()).toEqual(hard);
+    });
+});
