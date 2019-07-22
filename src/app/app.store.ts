ff96eb7da62746d28cf21d29e30fdb8e3eece89f
Julien Giovaresco <dev@giovaresco.fr>
2019-07-22 13:24:20 +0200

    feat: use redux to select the language

---
+++ src/app/app.store.ts
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducers } from './reducers';
+
+export const store = createStore(reducers, composeWithDevTools());
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import 'pixi';
 import 'p2';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { App } from './app/App';
+import { store } from './app/app.store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
