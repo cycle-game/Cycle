f2f153a45eabad000142e7138d796e0874f062bd
Gaëtan Maisse <gmaisse@solocal.com>
2019-08-04 09:39:12 +0200

    test: fix unit tests of language reducer

--- src/app/reducers/language.reducer.test.ts
+++ src/app/reducers/language.reducer.test.ts
@@ -4,12 +4,14 @@ import { languageReducer } from './language.reducer';
 describe('Language reducer', () => {
     it('provides an initial state', () => {
         expect(languageReducer(undefined, {} as any)).toEqual({
+            availableLanguages: [{ code: 'en', name: 'English' }, { code: 'fr', name: 'Français' }],
             selectedLanguage: '',
         });
     });
 
     it('set the languageReducer on SELECT_LANGUAGE', () => {
         expect(languageReducer(undefined, selectLanguage('fr'))).toEqual({
+            availableLanguages: [{ code: 'en', name: 'English' }, { code: 'fr', name: 'Français' }],
             selectedLanguage: 'fr',
         });
     });
