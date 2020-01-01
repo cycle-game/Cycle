c2a1830817587b9be2f63517caa7106dbd895f88
Gaëtan Maisse <gmaisse@solocal.com>
2020-01-01 19:48:16 +0100

    refactor: split parts of the Phaser game in multiples autonomous "games"

--- src/app/the-game/TheGame.tsx
+++ src/app/the-game/TheGame.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import { CycleGame } from '../../game';
+import { BASE_SIZE } from '../../game/variables';
+import { CycleStageEditorGame } from '../../game/CycleStageEditorGame';
+import { CycleScoreGame } from '../../game/CycleScoreGame';
+import { i18nService } from '../../i18n/I18nService';
 
 type TheGameProps = {};
 
 type TheGameState = {
-    game: CycleGame;
+    game?: CycleGame | CycleStageEditorGame | CycleScoreGame;
 };
 
 export class TheGame extends Component<TheGameProps, TheGameState> {
@@ -14,22 +18,40 @@ export class TheGame extends Component<TheGameProps, TheGameState> {
     constructor(props: TheGameProps) {
         super(props);
 
-        this.state = {
-            game: new CycleGame(TheGame.CANVAS_ID),
-        };
+        this.state = {};
     }
 
-    componentDidMount(): void {
-        this.state.game.start();
-    }
+    componentDidMount(): void {}
 
     render() {
+        const startGame = (game: CycleGame | CycleStageEditorGame | CycleScoreGame) => {
+            if (this.state.game) {
+                this.state.game.stop();
+            }
+
+            game.start();
+            this.setState({ game });
+        };
+
         const style = {
-            width: `${this.state.game.baseSize}px`,
-            height: `${this.state.game.baseSize}px`,
+            width: `${BASE_SIZE}px`,
+            height: `${BASE_SIZE}px`,
             margin: 'auto',
         };
 
-        return <div id={TheGame.CANVAS_ID} style={style} />;
+        return (
+            <div>
+                <button onClick={() => startGame(new CycleStageEditorGame(TheGame.CANVAS_ID))}>
+                    {i18nService.translate('Editeur')}
+                </button>
+                <button onClick={() => startGame(new CycleGame(TheGame.CANVAS_ID))}>
+                    {i18nService.translate('Jeu')}
+                </button>
+                <button onClick={() => startGame(new CycleScoreGame(TheGame.CANVAS_ID))}>
+                    {i18nService.translate('Scores')}
+                </button>
+                <div id={TheGame.CANVAS_ID} style={style} />
+            </div>
+        );
     }
 }
