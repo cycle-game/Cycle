5c0d5dcd4c3fab3a183260683dc732d689b63b11
Gaëtan Maisse <gmaisse@solocal.com>
2020-01-01 17:45:51 +0100

    chore: add types for `react-redux`

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,12 @@ import { useSelector } from 'react-redux';
 
 import { TheGame } from './the-game';
 import { SelectLanguage } from './containers';
+import { LanguageState } from './reducers';
 import { isEmpty } from 'lodash';
-import { i18nService } from '../i18n/I18nService';
 import './App.scss';
 
 export const App = () => {
-    const selectedLanguage = useSelector(state => state.language.selectedLanguage);
+    const selectedLanguage = useSelector((state: { language: LanguageState }) => state.language.selectedLanguage);
 
     let component = <SelectLanguage />;
     if (!isEmpty(selectedLanguage)) {
