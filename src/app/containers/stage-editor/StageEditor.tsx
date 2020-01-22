43e6db72507fb78afc6f83149a447ae27ab35376
Gaëtan Maisse <gmaisse@solocal.com>
2020-01-22 21:14:47 +0100

    refactor: handle ESC key event in StageEditor screen

--- src/app/containers/stage-editor/StageEditor.tsx
+++ src/app/containers/stage-editor/StageEditor.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import { BASE_SIZE } from '../../../game/variables';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
+import { useHistory } from 'react-router-dom';
 import { CycleStageEditorGame } from '../../../game/CycleStageEditorGame';
 import './StageEditor.scss';
 
@@ -11,13 +13,20 @@ const style = {
 };
 
 export const StageEditor: FunctionComponent = () => {
+    const history = useHistory();
+
     useEffect(() => {
         new CycleStageEditorGame(CANVAS_ID).start();
     }, []);
 
+    const backToMenu = () => {
+        history.push('/');
+    };
+
     return (
         <div className="StageEditor">
             <div id={CANVAS_ID} style={style} />
+            <KeyboardEventHandler handleKeys={['esc']} onKeyEvent={backToMenu} isExclusive={true} />
         </div>
     );
 };
