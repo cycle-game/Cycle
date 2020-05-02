6638e5630fe0de7771312d8da584c4d63464e7a4
Gaëtan Maisse <gmaisse@solocal.com>
2020-05-02 17:51:44 +0200

    chore: upgrade to Storybook 6
    - Move config to the new `main.js` mono config file
    - Remove unneeded packages
    - Upgrade packages to `6.0.0-beta.1`
    - Use `addon-docs` instead of deprecated `addon-info` (it has a peerDep on `react-is`)

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+module.exports = {
+    stories: ['../src/**/*.stories.tsx'],
+    addons: [
+        {
+            name: '@storybook/addon-docs',
+            options: { configureJSX: true },
+        },
+        '@storybook/addon-actions',
+    ],
+    webpackFinal: config => {
+        config.module.rules.push({
+            test: /\.tsx?$/,
+            use: [
+                { loader: require.resolve('ts-loader') },
+                { loader: require.resolve('react-docgen-typescript-loader') },
+            ],
+        });
+
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+        });
+
+        config.resolve.extensions.push('.ts', '.tsx');
+        return config;
+    },
+};
