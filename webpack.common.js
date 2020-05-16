5390c2440b31a9e6ef805b0d11d5d4ee65f3bc95
Gaëtan Maisse <gmaisse@solocal.com>
2020-05-16 21:58:10 +0200

    chore: bump dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -10,83 +10,81 @@ const pixi = path.join(phaserModule, 'build/custom/pixi.js');
 const p2 = path.join(phaserModule, 'build/custom/p2.js');
 
 module.exports = {
-  entry: {
-    main: './src/index.tsx',
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].[chunkhash].js'
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          keep_classnames: true,
-          keep_fnames: true
-        }
-      })
-    ],
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          enforce: true,
-          chunks: 'all'
-        }
-      }
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Cycle',
-      template: './src/index.html'
-    }),
-    new CopyPlugin([
-      { from: 'src/resources', to: 'resources' },
-    ])
-  ],
-  module: {
-    rules: [
-      { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
-      { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-      { test: /p2\.js/, use: ['expose-loader?p2'] },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      'phaser-ce': phaser,
-      'pixi': pixi,
-      'p2': p2
+    entry: {
+        main: './src/index.tsx',
     },
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].[chunkhash].js',
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_classnames: true,
+                    keep_fnames: true,
+                },
+            }),
+        ],
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    enforce: true,
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Cycle',
+            template: './src/index.html',
+        }),
+        new CopyPlugin({ patterns: [{ from: 'src/resources', to: 'resources' }] }),
+    ],
+    module: {
+        rules: [
+            { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
+            { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+            { test: /p2\.js/, use: ['expose-loader?p2'] },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader', // translates CSS into CommonJS
+                    'sass-loader', // compiles Sass to CSS, using Node Sass by default
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            'phaser-ce': phaser,
+            pixi: pixi,
+            p2: p2,
+        },
+        extensions: ['.ts', '.tsx', '.js'],
+    },
 };
