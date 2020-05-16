2ed4cc533874d9f0877168cca1dbc727ae0a894f
Gaëtan Maisse <gmaisse@solocal.com>
2020-05-16 17:50:20 +0200

    refactor: convert TheGame to FunctionComponent

--- src/app/the-game/TheGame.tsx
+++ src/app/the-game/TheGame.tsx
@@ -1,5 +1,5 @@
 import './TheGame.scss';
-import React, { Component } from 'react';
+import React from 'react';
 
 import { PlayerProgression } from '../../game/variables';
 import { i18nService } from '../../i18n/I18nService';
@@ -10,51 +10,47 @@ import { StageEditor } from '../containers/stage-editor/StageEditor';
 import { GameContainer } from '../containers/game-container';
 import { RulesDisplayer } from '../containers/rules-displayer';
 
-export class TheGame extends Component<{}, {}> {
-    render() {
-        return (
-            <div className="TheGame">
-                <BrowserRouter>
-                    <Switch>
-                        <Route path="/scores">
-                            <ScoresDisplayer />
-                        </Route>
-                        <Route path="/stage-editor">
-                            <StageEditor />
-                        </Route>
-                        <Route path="/game">
-                            <GameContainer />
-                        </Route>
-                        <Route path="/rules">
-                            <RulesDisplayer />
-                        </Route>
-                        <Route path="/">
-                            <div className="menu">
-                                <img className="logo" src="resources/tiles/logo-without-c.png" />
-                                <Link to="/rules" style={{ textDecoration: 'none' }}>
-                                    <div className="item">{i18nService.translate('Jeu')}</div>
-                                </Link>
+export const TheGame = () => (
+    <div className="TheGame">
+        <BrowserRouter>
+            <Switch>
+                <Route path="/scores">
+                    <ScoresDisplayer />
+                </Route>
+                <Route path="/stage-editor">
+                    <StageEditor />
+                </Route>
+                <Route path="/game">
+                    <GameContainer />
+                </Route>
+                <Route path="/rules">
+                    <RulesDisplayer />
+                </Route>
+                <Route path="/">
+                    <div className="menu">
+                        <img className="logo" src="resources/tiles/logo-without-c.png" />
+                        <Link to="/rules" style={{ textDecoration: 'none' }}>
+                            <div className="item">{i18nService.translate('Jeu')}</div>
+                        </Link>
 
-                                <Link to="/scores" style={{ textDecoration: 'none' }}>
-                                    <div className="item">{i18nService.translate('Scores')}</div>
-                                </Link>
+                        <Link to="/scores" style={{ textDecoration: 'none' }}>
+                            <div className="item">{i18nService.translate('Scores')}</div>
+                        </Link>
 
-                                <Link to="/stage-editor" style={{ textDecoration: 'none' }}>
-                                    <div className="item">{i18nService.translate('Editeur')}</div>
-                                </Link>
-                                <div
-                                    className="item"
-                                    onClick={() => {
-                                        reset(PlayerProgression);
-                                    }}
-                                >
-                                    {i18nService.translate('RaZ')}
-                                </div>
-                            </div>
-                        </Route>
-                    </Switch>
-                </BrowserRouter>
-            </div>
-        );
-    }
-}
+                        <Link to="/stage-editor" style={{ textDecoration: 'none' }}>
+                            <div className="item">{i18nService.translate('Editeur')}</div>
+                        </Link>
+                        <div
+                            className="item"
+                            onClick={() => {
+                                reset(PlayerProgression);
+                            }}
+                        >
+                            {i18nService.translate('RaZ')}
+                        </div>
+                    </div>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    </div>
+);
